d59c71c5598a9b6983544db2a08219754b73c204
Trần Tuấn Cảnh <39213510+connortran216@users.noreply.github.com>
2020-09-07 14:30:30 +0700

    Add files via upload
    Re-arrange code to improve the processing speed per frame

--- real_time_cv2.py
+++ real_time_cv2.py
@@ -18,26 +18,12 @@ from os.path import isdir
 import matplotlib.pyplot as plt
 from keras.models import load_model
 
-#Import functions
-# from face_extract import extract_face, load_faces, load_dataset
-# from face_embedding import get_embedding
+# #Import functions
+from sort import *
 
 
 # extract a single face from a given photograph
 def extract_face(frame, results, required_size=(160, 160)):
-	# # load image from file
-	# image = Image.open(filename)
-	
-	# convert to RGB, if needed
-	#image = image.convert('RGB')
-	#image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-	#print("image: ", image)
-	
-	# convert to array
-	#pixels = asarray(image)
-	#print("pixels: ", pixels)
-	
-
 	# extract the bounding box from the first face
 	x1, y1, width, height = results[0]['box']
 	
@@ -54,40 +40,7 @@ def extract_face(frame, results, required_size=(160, 160)):
 	face_array = asarray(image)
 	return face_array
 
-# # load images and extract faces for all images in a directory
-# def load_faces(directory):
-# 	faces = list()
-# 	# enumerate files
-# 	for filename in listdir(directory):
-# 		# path
-# 		path = directory + filename
-# 		# get face
-# 		face = extract_face(path)
-# 		# store
-# 		faces.append(face)
-# 	return faces
 
-# # load a dataset that contains one subdir for each class that in turn contains images
-# def load_dataset(directory):
-# 	X = list()
-# 	# enumerate folders, on per class
-# 	for subdir in listdir(directory):
-# 		# path
-# 		path = directory + subdir + '/'
-# 		print(path)
-# 		# skip any files that might be in the dir
-# 		if not isdir(path):
-# 			continue
-# 		# load all faces in the subdirectory
-# 		faces = load_faces(path)
-# 		# create labels
-# 		labels = [subdir for _ in range(len(faces))]
-# 		# summarize progress
-# 		print('>loaded %d examples for class: %s' % (len(faces), subdir))
-# 		# store
-# 		X.extend(faces)
-# 		#y.extend(labels)
-# 	return asarray(X)#, asarray(y)
 
 # get the face embedding for one face
 def get_embedding(model, face_pixels):
@@ -109,9 +62,20 @@ print('Loaded Model')
 # load the model from disk
 loaded_model = pickle.load(open('svm_model.sav', 'rb'))
 
-# load face embeddings
-data = load('5-celebrity-faces-embeddings.npz')
-trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+
+testy = ['ben_afflek', 'elton_john', 'jerry_seinfeld', 'madonna', 'mindy_kaling']
+print("Test t label: ", testy)
+# label encode targets
+out_encoder = LabelEncoder()
+out_encoder.fit(testy)
+testy = out_encoder.transform(testy)
+
+
+
+#create instance of SORT
+mot_tracker = Sort() 
+# create the detector, using default weights
+detector = MTCNN()
 
 video_src = 'ben1.mp4'
 cap = cv2.VideoCapture(video_src)
@@ -119,17 +83,14 @@ while True:
 	#Capture frame-by-frame
 	ret, frame = cap.read()
 	#print(frame)
-	# load the photo and extract the face
-	#testX_faces, testy_faces = load_dataset('./images/')
 
-		# create the detector, using default weights
-	detector = MTCNN()
-	
 	# detect faces in the image
 	results = detector.detect_faces(frame)
 	#print("results: ", results)
-	
+
 	if results != []:
+		print("Box: ", results[0]["box"])
+		print("Confidence: ", results[0]["confidence"])
 		for person in results:
 			bounding_box = person['box']
 			keypoints = person['keypoints']
@@ -139,73 +100,60 @@ while True:
 						  (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
 						  (0,155,255),
 						  2)
-	
-			# cv2.circle(frame,(keypoints['left_eye']), 2, (0,155,255), 2)
-			# cv2.circle(frame,(keypoints['right_eye']), 2, (0,155,255), 2)
-			# cv2.circle(frame,(keypoints['nose']), 2, (0,155,255), 2)
-			# cv2.circle(frame,(keypoints['mouth_left']), 2, (0,155,255), 2)
-			# cv2.circle(frame,(keypoints['mouth_right']), 2, (0,155,255), 2)
 
 
-			face = extract_face(frame, results)
-			faces = list()
-			faces.append(face)
-			testX_faces = list()
-			testX_faces.extend(faces)
-			#print(testX_faces, testy_faces)
+		face = extract_face(frame, results)
+		# load face embeddings
+		# convert each face in the test set to an embedding
+		newTestX = list()
+		# for face_pixels in testX_faces:
+		# 	embedding = get_embedding(model, face_pixels)
+		# 	newTestX.append(embedding)
+
+		embedding = get_embedding(model, face)
+		newTestX.append(embedding)
+
+		newTestX = asarray(newTestX)
+		#print(newTestX.shape)
+
+		# normalize input vectors
+		in_encoder = Normalizer(norm='l2')
+		testX = in_encoder.transform(newTestX)
+		#print(testX)
 
 
-			# load face embeddings
-			# convert each face in the test set to an embedding
-			newTestX = list()
-			for face_pixels in testX_faces:
-				embedding = get_embedding(model, face_pixels)
-				newTestX.append(embedding)
-			newTestX = asarray(newTestX)
-			#print(newTestX.shape)
 
-			# normalize input vectors
-			in_encoder = Normalizer(norm='l2')
-			testX = in_encoder.transform(newTestX)
+		# test model on a random example fromq the test dataset
+		# selection = choice([i for i in range(newTestX.shape)])
+		# print("selection: ", newTestX.shape[0])
+		#random_face_pixels = testX_faces[selection]
+		random_face_emb = testX[0]
 
-			# label encode targets
-			out_encoder = LabelEncoder()
-			out_encoder.fit(testy)
-			testy = out_encoder.transform(testy)
+		# random_face_class = testy[selection]
+		# random_face_name = out_encoder.inverse_transform([random_face_class])
+		
 
-			# test model on a random example fromq the test dataset
-			selection = choice([i for i in range(newTestX.shape[0])])
-			random_face_pixels = testX_faces[selection]
-			random_face_emb = testX[selection]
-			random_face_class = testy[selection]
-			random_face_name = out_encoder.inverse_transform([random_face_class])
+		# prediction for the face
+		samples = expand_dims(random_face_emb, axis=0)
+		yhat_class = loaded_model.predict(samples)
+		yhat_prob = loaded_model.predict_proba(samples)
 
+		# get name
+		class_index = yhat_class[0]
+		class_probability = yhat_prob[0,class_index] * 100
+		predict_names = out_encoder.inverse_transform(yhat_class)
+		print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
+		#print('Expected: %s' % random_face_name[0])
 
+		font = cv2.FONT_HERSHEY_SIMPLEX
+		color = (255,255,255)
+		stroke = 2
+		if class_probability > 65:
+			cv2.putText(frame, str(predict_names[0]) + ": " + str(class_probability), 
+						(int(bounding_box[0]), int(bounding_box[1])), font, 1, color, stroke, cv2.LINE_AA)
 			
-
-			# prediction for the face
-			samples = expand_dims(random_face_emb, axis=0)
-			yhat_class = loaded_model.predict(samples)
-			yhat_prob = loaded_model.predict_proba(samples)
-
-			# get name
-			class_index = yhat_class[0]
-			class_probability = yhat_prob[0,class_index] * 100
-			predict_names = out_encoder.inverse_transform(yhat_class)
-			print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
-			print('Expected: %s' % random_face_name[0])
-
-			font = cv2.FONT_HERSHEY_SIMPLEX
-			color = (255,255,255)
-			stroke = 2
-
-			cv2.putText(frame, str(predict_names[0]), (200, 200), font, 1, color, stroke, cv2.LINE_AA)
-
+		fps = int(cap.get(cv2.CAP_PROP_FPS))
+		cv2.putText(frame, "FPS: " + str(fps), (100,100), font, 1, color, stroke, cv2.LINE_AA)
 	cv2.imshow('video', frame)
 	if cv2.waitKey(20) & 0xFF == ord("q"):
 		break
-	# # plot for fun
-	# pyplot.imshow(random_face_pixels)
-	# title = '%s (%.3f)' % (predict_names[0], class_probability)
-	# pyplot.title(title)
-	# pyplot.show()
